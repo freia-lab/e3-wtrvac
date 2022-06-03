998ca6f23b7cd0f580dde2175c999872ae5b844c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-06-03 11:12:49 +0200

    Updated opi.

--- opi/scripts/openHeater2.js
+++
@@ -1,8 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-
-
-mac=DataUtil.createMacrosInput(false);
-
-mac.put("Heater2", widget.getPropertyValue("name"));
-
-ScriptUtil.openOPI(widget, "../helper-opi-files/CstatH-Heater2.opi", 7, mac);
